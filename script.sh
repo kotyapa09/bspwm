56418a83466d49c13c5e2bd28618e3dbcf1ed027
kotyapa09 <cibrikovdima163@gmail.com>
2025-01-29 15:36:45 +0700

    first commit

---
+++ script.sh
@@ -0,0 +1,4 @@
+curl https://raw.githubusercontent.com/blackploit/hash-identifier/refs/heads/master/hash-id.py > ~/bin/hash-id.py
+
+sudo git clone https://github.com/00xBAD/kali-wordlists.git /usr/share/wordlists
+sudo git clone https://github.com/danielmiessler/SecLists /usr/share/wordlists/SecLists
