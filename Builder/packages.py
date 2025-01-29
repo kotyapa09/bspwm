4097a66cc64147808f9b5af4a83adb04386d1fb6
kotyapa09 <cibrikovdima163@gmail.com>
2025-01-29 15:37:21 +0700

    Builder added

---
+++ Builder/packages.py
@@ -0,0 +1,31 @@
+
+
+BASE_PACKAGES = [
+    "tumbler", "ffmpegthumbnailer", "lsd", "alacritty", "bat", "brightnessctl", "calc",
+    "automake", "blueman", "bluez", "bluez-utils", "dunst", "fakeroot", "feh", "firefox",
+    "fish", "dpkg", "gcc", "gedit", "git", "gnu-netcat", "htop", "btop", "lxappqearance",
+    "mat2", "mpd", "mpv", "thunar", "ncmpcpp", "neofetch", "network-manager-applet", "nitrogen",
+    "pamixer", "papirus-icon-theme", "pavucontrol", "polybar", "autoconf", "mpc", "pulseaudio",
+    "pulseaudio-alsa", "python-pyalsa", "ranger", "redshift", "reflector", "rofi", "rofi-calc", "calcurse",
+    "rofi-emoji", "scrot", "sudo", "slop", "tree", "unrar", "zip", "unzip", "uthash", "xarchiver",
+    "xfce4-power-manager", "xfce4-settings", "xorg-xbacklight", "zathura", "zathura-djvu", "zathura-pdf-mupdf",
+    "cmake", "clang", "gzip", "imagemagick", "make", "openssh", "pulseaudio-bluetooth", "shellcheck",
+    "vlc", "usbutils", "picom", "networkmanager-openvpn", "alsa-plugins", "alsa-tools", "alsa-utils", "ffmpeg",
+    "p7zip", "gparted", "sshfs", "openvpn", "xclip", "gpick", "wget", "ueberzug", "netctl", "light", "libreoffice",
+    "breeze", "vulkan-intel", "intel-ucode", "ttf-jetbrains-mono", "ttf-jetbrains-mono-nerd", "ttf-fira-code",
+    "ttf-iosevka-nerd", "flameshot", "ghex", "chromium", "obsidian", "telegram-desktop", "python-pip", "docker"
+]
+
+DEV_PACKAGES = [
+    "nmap", "hashcat", "exiftool", "aircrack-ng"
+]
+
+AUR_PACKAGES = [
+    "gobuster", "steghide", "binwalk"
+]
+
+GNOME_OFFICIAL_TOOLS = [
+    "evince", "gnome-calculator", "gnome-disk-utility", "gucharmap",
+    "gthumb", "gnome-clocks"
+]
+
